77ad5b5ab3c5242ef404ddb95666400910dcfefb
WozMit <jaredleon.223@gmail.com>
2018-04-03 16:17:23 -0500

    Modes of operation separated

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # AES Cipher
-A python script for file encryption/decryption with AES (i.e., 128, 192 and 256) in CBC mode.
+A python script for Authenticated encryption [AE](https://en.wikipedia.org/wiki/Authenticated_encryption) with AES (i.e., 128, 192 and 256) in [GCM](https://en.wikipedia.org/wiki/Galois/Counter_Mode) mode of operation.
 
 ## Getting Started
 
 ### Built With
 * Python 3.6.4
-* PyCrypto 2.6.1 [official site](https://www.dlitz.net/software/pycrypto/), [PyPI](https://pypi.python.org/pypi/pycrypto), [Source Repo (GitHub)](https://github.com/dlitz/pycrypto). Available via `conda` or `pip`.
+* PyCryptodome 3.5.1 [official site](http://pycryptodome.readthedocs.io/en/latest/src/introduction.html), [PyPI](https://pypi.python.org/pypi/pycryptodome), [Source Repo (GitHub)](https://github.com/Legrandin/pycryptodome). Available via `pip install pycryptodome`.
 
 ### Documentation
-* [PyCrypto API Documentation](https://www.dlitz.net/software/pycrypto/api/2.6/)
-* [General overview of PyCrypto](https://www.dlitz.net/software/pycrypto/doc/)
-* Project based on [this](http://www.letscodepro.com/encryption-in-python-using-pycrypto/) blog
+* [PyCryptodome’s documentation](https://www.pycryptodome.org/en/latest/)
+* [PyCryptodome API documentation](http://pycryptodome.readthedocs.io/en/latest/src/api.html)
+* Project based on [this](https://github.com/wolf43/AES-GCM-example) code.
 
 ### Author
 WozMit
--- main.py
+++ main.py
@@ -1,79 +1,16 @@
-import os;
-from Crypto import Random;
-from Crypto.Cipher import AES;
 import tkinter;
 
-class Encryptor:
-    def __init__(self, key):
-        self.key = key;
+root = tkinter.Tk();
+root.title("AES 256");
+root.geometry('250x200');
 
-    def pad(self, s):
-        return s + b"\0" * ( AES.block_size - len(s) % AES.block_size );
+btn_folder = tkinter.Button(root, text = "Select folder", command=2);
+btn_folder.pack();
 
-    def encrypt_bits(self, message, key):
-        message = self.pad(message);
-        iv = Random.new().read(AES.block_size);
-        cipher = AES.new(key, AES.MODE_CBC, iv);
-        return iv + cipher.encrypt(message);
+btn_encrypt = tkinter.Button(root, text = "Encrypt folder");
+btn_encrypt.pack();
 
-    def encrypt_file(self, file_name):
-        with open(file_name, 'rb') as file:
-            text = file.read();
-            file.close();
-        encoded = self.encrypt_bits(text, self.key);
-        with open(file_name + ".woz", 'wb') as file:
-            file.write(encoded);
-            file.close();
-        os.remove(file_name);
+btn_decrypt = tkinter.Button(root, text = "Decrypt folder");
+btn_decrypt.pack();
 
-    def decrypt_bits(self, encoded_message, key):
-        iv = encoded_message[:AES.block_size];
-        cipher = AES.new(key, AES.MODE_CBC, iv);
-        text = cipher.decrypt(encoded_message[AES.block_size:]);
-        return text.rstrip(b"\0");
-
-    def decrypt_file(self, file_name):
-        with open(file_name, 'rb') as file:
-            encoded = file.read();
-            file.close();
-        text = self.decrypt_bits(encoded, self.key);
-        with open(file_name[:-4], 'wb') as file:
-            file.write(text);
-            file.close();
-        os.remove(file_name);
-
-    def get_file_list(self, location, avoid_files=[]):
-        file_list = [];
-        for dir_name, sub_dir_list, dir_file_list in os.walk(location):
-            if(".git" in sub_dir_list):
-                    sub_dir_list.remove(".git");
-            for file in dir_file_list:
-                if(file not in avoid_files):
-                    file_list.append(os.path.join(dir_name, file));
-        return file_list;
-
-
-    def encrypt_location(self, location=os.path.dirname(os.path.realpath(__file__))):
-        avoid_files = [__file__, "README.md", "enc-dec.py"];
-        file_list = self.get_file_list(location, avoid_files);
-        for file in file_list:
-            self.encrypt_file(file);
-
-    def decrypt_location(self, location=os.path.dirname(os.path.realpath(__file__))):
-        avoid_files = [__file__, "README.md", "enc-dec.py"];
-        file_list = self.get_file_list(location, avoid_files);
-        for file in file_list:
-            self.decrypt_file(file);
-
-key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e';
-encryptor = Encryptor(key);
-#encryptor.encrypt_location();
-#encryptor.decrypt_location();
-
-#root = tkinter.Tk();
-#root.title("AES 256");
-
-#load_file_btn = tkinter.Button(root, text = "Load file", command=encryptor.pad);
-#load_file_btn.pack();
-
-#root.mainloop();
+root.mainloop();
