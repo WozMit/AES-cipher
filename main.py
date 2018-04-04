52d558d87d2222feafe9c5c0b99126537d208256
WozMit <jaredleon.223@gmail.com>
2018-04-04 00:15:48 -0500

    Basic form created

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 from aes_gcm import Encryptor;
 import time as tm;
-#import tkinter;
+import tkinter;
+from tkinter import *;
+from tkinter import filedialog;
+from tkinter import messagebox;
+
+def load_folder():
+	folder = filedialog.askdirectory();
+	txbLocation.delete(0, END);
+	txbLocation.insert(0, folder);
+	return folder;
 
 start_time = tm.time();
 
-#root = tkinter.Tk();
-#root.title("AES");
-#root.geometry('250x200');
+root = tkinter.Tk();
+root.title("AES");
+root.geometry('250x200');
 
-#btn_folder = tkinter.Button(root, text = "Select folder", command=2);
-#btn_folder.pack();
+btnSearch = tkinter.Button(root, text = "Select folder", command=load_folder);
+btnSearch.pack();
 
-#btn_encrypt = tkinter.Button(root, text = "Encrypt folder");
-#btn_encrypt.pack();
+txbLocation = Entry(root, width=25);
+txbLocation.pack(side=TOP);
 
-#btn_decrypt = tkinter.Button(root, text = "Decrypt folder");
-#btn_decrypt.pack();
+btnEncrypt = tkinter.Button(root, text = "Encrypt folder");
+btnEncrypt.pack();
 
-#root.mainloop();
+btnDecrypt = tkinter.Button(root, text = "Decrypt folder");
+btnDecrypt.pack();
+
+root.mainloop();
 
 password = "awesome password";
+location = "D:\Documentos\Python\AES Cipher\X Files";
 encriptor = Encryptor(256, password);
-#encriptor.encrypt_location("D:\Documentos\Python\AES Cipher\X Files");
-#encriptor.decrypt_location("D:\Documentos\Python\AES Cipher\X Files");
+#encriptor.encrypt_location(location);
+#encriptor.decrypt_location(location);
 
 print("\nTotal time = "+str(tm.time()-start_time)+" s");
