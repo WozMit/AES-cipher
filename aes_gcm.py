e8d8f06194393f4fb5598a875779ab28247b6d99
WozMit <jaredleon.223@gmail.com>
2018-04-03 22:05:23 -0500

    GCM file encryption working

--- aes_gcm.py
+++ aes_gcm.py
@@ -1,33 +1,60 @@
 from Crypto.Cipher import AES;
+from Crypto.Protocol.KDF import scrypt;
 from Crypto.Random import get_random_bytes;
+import os;
 
-#key = b'\x1d\x89x~ NX\x0b\xdc\x96M\xf1%q\x95\x9a';
-#key = b'8\xa9\xe8\xf2\x07\x07\x83F\xef\xb1\xe6p"<\xc7C\xff\xa3kd\xb0\xd76S';
-key = b'l\xdd2\xdf\xe5\xc7\x7f\xc8\xe3\xfbE\xffgt\x1fc\x11\xbb\xfe\xc9\x08\xf2>Zsf\x10\x1eS\xedDA';
+class Encryptor:
+    def __init__(self, key_len, password):
+        self.key_len = key_len;
+        self.password = password;
 
-plain_text = b'Some important text';
+    def encrypt_bytes(self, plain_text):
+        salt = get_random_bytes(8);
+        key = scrypt(self.password, salt=salt, key_len=self.key_len//8, N=16384, r=8, p=1);
+        nonce = get_random_bytes(16);
+        cipher = AES.new(key=key, mode=AES.MODE_GCM, nonce=nonce, mac_len=16);
+        cipher.update(b'WozMit');
+        ciphertext, tag = cipher.encrypt_and_digest(plain_text);
+        return (self.key_len//64).to_bytes(1, byteorder='big') + salt + nonce + tag + ciphertext;
 
-nonce = get_random_bytes(16);
-aad = b'WozMit';
-cipher = AES.new(key=key, mode=AES.MODE_GCM, nonce=nonce, mac_len=16);
-cipher.update(aad);
-ciphertext, tag = cipher.encrypt_and_digest(plain_text);
-print("Original text:", plain_text);
-print("Ciphertext:", ciphertext);
-print("Tag:", tag);
-print("=============================================");
+    def encrypt_file(self, file_name):
+        with open(file_name, 'rb') as file:
+            plain_text = file.read();
+            file.close();
+        ciphermessage = self.encrypt_bytes(plain_text);
+        with open(file_name + ".woz", 'wb') as file:
+            file.write(ciphermessage);
+            file.close();
+        os.remove(file_name);
 
-r_key = b'l\xdd2\xdf\xe5\xc7\x7f\xc8\xe3\xfbE\xffgt\x1fc\x11\xbb\xfe\xc9\x08\xf2>Zsf\x10\x1eS\xedDA';
-r_nonce = nonce;
-r_aad = aad;
-r_ciphertext = ciphertext;
+    def decrypt_bytes(self, ciphermessage):
+        key_len = int.from_bytes(ciphermessage[:1], byteorder='big')*64;
+        salt = ciphermessage[1:9];
+        nonce = ciphermessage[9:25];
+        tag = ciphermessage[25:41];
+        ciphertext = ciphermessage[41:];
+        key = scrypt(self.password, salt=salt, key_len=key_len//8, N=16384, r=8, p=1);
+        cipher = AES.new(key=key, mode=AES.MODE_GCM, nonce=nonce, mac_len=16);
+        cipher.update(b'WozMit');
+        try:
+            plain_text = cipher.decrypt_and_verify(ciphertext, tag);
+            return plain_text;
+        except:
+            return False;
 
+    def decrypt_file(self, file_name):
+        with open(file_name, 'rb') as file:
+            ciphermessage = file.read();
+            file.close();
+        plain_text = self.decrypt_bytes(ciphermessage);
+        if(plain_text == False):
+            print(file_name+" could not be decrypted. Wrong password or corrupted file.");
+        else:
+            with open(file_name[:-4], 'wb') as file:
+                file.write(plain_text);
+                file.close();
+            os.remove(file_name);
 
-cipher = AES.new(key=r_key, mode=AES.MODE_GCM, nonce=r_nonce, mac_len=16);
-cipher.update(r_aad);
-try:
-    plain_text = cipher.decrypt_and_verify(r_ciphertext, tag);
-    print("MAC validated: Data was encrypted by someone with the shared secret passphrase");
-    print("Decrypted succesfully:", plain_text);
-except:
-    print("MAC validation failed during decryption. No authentication gurantees on this ciphertext");
+encriptor = Encryptor(256, "awesome password");
+#encriptor.encrypt_file("D:\Documentos\Python\AES Cipher\X Files\calamardo.jpg");
+#encriptor.decrypt_file("D:\Documentos\Python\AES Cipher\X Files\calamardo.jpg.woz");
